1ad9ca3c908497438e4b791d39874f5a57f1e0ee
David Josiger <david.josiger@gmail.com>
2022-12-03 12:40:47 +0100

    day3

--- day3/solution.py
+++ day3/solution.py
@@ -16,7 +16,6 @@ def part2(input) -> int:
     with open(input) as f:
         for l in f:
             listed.append(l.strip())
-        index = 0
         for i in range(3, len(listed) + 3, 3):
             intersection = set(listed[i - 3]) & set(listed[i - 2]) & set(listed[i - 1])
             total += string.ascii_letters.index(min(intersection)) + 1
